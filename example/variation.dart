3c5a721d43473f3ae4aab2d26899c2e60b828801
Mihail <miskadl09@gmail.com>
2023-06-17 20:50:31 +0200

    Initialise logic

--- example/variation.dart
+++ example/variation.dart
@@ -1,10 +1,8 @@
 // ignore_for_file: avoid_print
 
 import 'dart:async';
-import 'dart:developer';
 
 import 'package:midjourney_client/midjourney_client.dart' as midjourney_client;
-import 'package:midjourney_client/src/core/midjourney/model/midjourney_message.dart';
 
 import 'env.dart';
 
@@ -16,15 +14,11 @@ Future<void> main(List<Object> arguments) async {
     loggerLevel: midjourney_client.MLoggerLevel.debug,
   );
 
+  await client.init();
+
   final imagine = client.imagine('Cat with sword')..listen(print);
 
   final result = await imagine.last;
-
-  if (result is! MidjourneyMessage$ImageFinish) {
-    throw Exception('Expected ImageMessage\$Finish but got $imagine');
-  }
-
-  log(result.toString());
 
   client.variation(result, 1).listen(print);
   client.variation(result, 2).listen(print);
